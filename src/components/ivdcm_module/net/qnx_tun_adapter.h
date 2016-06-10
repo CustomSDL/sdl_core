a609833d115e34483d4d443278cae44872a3ea46
Konstantin Kolodiy <KKolodiy@luxoft.com>
2016-06-10 11:11:33 +0300

    Add empty TUN adapters for QNX and Linux

---
+++ src/components/ivdcm_module/net/qnx_tun_adapter.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SRC_COMPONENTS_IVDCM_MODULE_NET_QNX_TUN_ADAPTER_H_
+#define SRC_COMPONENTS_IVDCM_MODULE_NET_QNX_TUN_ADAPTER_H_
+
+#include <string>
+
+#include "net/tun_adapter_interface.h"
+
+namespace net {
+
+class QnxTunAdapter : public TunAdapterInterface {
+ public:
+  explicit QnxTunAdapter(const std::string& nic);
+  virtual int Create();
+  virtual void Destroy(int id);
+  virtual bool SetAddress(int id, const std::string& value);
+  virtual bool GetAddress(int id, std::string *value);
+  virtual bool SetDestinationAddress(int id, const std::string& value);
+  virtual bool GetDestinationAddress(int id, std::string *value);
+  virtual bool SetNetmask(int id, const std::string& value);
+  virtual bool GetNetmask(int id, std::string *value);
+  virtual bool SetFlags(int id, int value);
+  virtual bool GetFlags(int id, int *value);
+
+ private:
+  const std::string nic_;
+};
+
+}  // namespace net
+
+#endif  // SRC_COMPONENTS_IVDCM_MODULE_NET_QNX_TUN_ADAPTER_H_
