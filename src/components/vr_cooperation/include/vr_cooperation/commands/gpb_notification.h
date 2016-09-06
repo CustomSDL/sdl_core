74ce224ea625a61b811051d50c22890aa2b4cdd4
Thinh Tran <tsthinh@luxoft.com>
2016-09-06 18:16:04 +0700

    REVSDL-2505 Implement GPB nofitications
    add GPBNofification class and OnRegisterServiceNotification class

---
+++ src/components/vr_cooperation/include/vr_cooperation/commands/gpb_notification.h
@@ -0,0 +1,105 @@
+/*
+ Copyright (c) 2016, Ford Motor Company
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following
+ disclaimer in the documentation and/or other materials provided with the
+ distribution.
+
+ Neither the name of the Ford Motor Company nor the names of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SRC_COMPONENTS_VR_COOPERATION_INCLUDE_VR_COOPERATION_COMMANDS_GPB_NOTIFICATION_H_
+#define SRC_COMPONENTS_VR_COOPERATION_INCLUDE_VR_COOPERATION_COMMANDS_GPB_NOTIFICATION_H_
+
+#include "application_manager/message.h"
+#include "vr_cooperation/commands/command.h"
+#include "vr_cooperation/interface/hmi.pb.h"
+
+namespace vr_cooperation {
+class VRModule;
+
+namespace commands {
+
+/**
+ * @brief GPB message command class for notifications
+ */
+class GPBNotification : public Command {
+ public:
+  /**
+   * @brief GPBNotification class constructor
+   * @param parent VRModule pointer
+   * @param message Message from mobile
+   **/
+  GPBNotification(VRModule* parent,
+                  const application_manager::MessagePtr& message);
+
+  /**
+   * @brief GPBNotification class destructor
+   */
+  virtual ~GPBNotification();
+
+  /**
+   * @brief GPBNotification on timeout reaction
+   */
+  virtual void OnTimeout();
+
+  /**
+   * @brief run notification
+   */
+  virtual void Run();
+
+ protected:
+  /**
+   * @brief send notification to HMI
+   * @param message GPB message to send
+   */
+  void SendNotification(vr_hmi_api::ServiceMessage& message);
+
+  /**
+   * @brief Interface method that executes specific logic of children classes
+   */
+  virtual void Execute() = 0;
+
+  /**
+   * @brief Returns parent VRModule
+   */
+  VRModule* parent() { return parent_; }
+
+  /**
+   * @brief Returns JSON message
+   */
+  const application_manager::MessagePtr& message() { return message_; }
+
+ private:
+  application_manager::MessagePtr message_;
+
+  VRModule* parent_;
+};
+
+}  // namespace commands
+
+}  // namespace vr_cooperation
+
+#endif  // SRC_COMPONENTS_VR_COOPERATION_INCLUDE_VR_COOPERATION_COMMANDS_GPB_NOTIFICATION_H_
