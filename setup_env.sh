3578915847fdd5a3dac0d17fdc9783bb4c1d0b99
Konstantin Kolodiy <KKolodiy@luxoft.com>
2016-10-17 17:16:05 +0300

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -4,7 +4,6 @@
 #
 
 # Install build tools
-sudo apt-get install git
 sudo apt-get install cmake
 sudo apt-get install automake autoconf
 sudo apt-get install g++
@@ -12,8 +11,3 @@ sudo apt-get install g++
 # Install libraries
 sudo apt-get install libsqlite3-dev
 sudo apt-get install libssl-dev
-
-# Create some links to build of protobuf
-ln -s aclocal-1.15 aclocal-1.14
-ln -s automake-1.15 automake-1.14
-
