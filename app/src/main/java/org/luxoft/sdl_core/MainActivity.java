85e36bbc3acd582f086eff2ce37b07d1dc7e037a
Andrii Kalinich <AKalinich@luxoft.com>
2021-02-18 20:38:51 -0500

    Update MainActivity

--- app/src/main/java/org/luxoft/sdl_core/MainActivity.java
+++ app/src/main/java/org/luxoft/sdl_core/MainActivity.java
@@ -1,30 +1,65 @@
 package org.luxoft.sdl_core;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText("Test");
+        final Button start_sdl = findViewById(R.id.start_sdl_button);
+        final Button stop_sdl = findViewById(R.id.stop_sdl_button);
+
+        start_sdl.setEnabled(true);
+        stop_sdl.setEnabled(false);
+
+        start_sdl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (startSDL()) {
+                    start_sdl.setEnabled(false);
+                    stop_sdl.setEnabled(true);
+                }
+            }
+        });
+
+        stop_sdl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start_sdl.setEnabled(true);
+                stop_sdl.setEnabled(false);
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private boolean startSDL() {
+        try {
+            System.loadLibrary("emhashmap");
+            System.loadLibrary("bson");
+            System.loadLibrary("smartDeviceLinkCore");
+        } catch (UnsatisfiedLinkError e) {
+            showToastMessage("Failed to load the library: " + e.getMessage());
+            return false;
+        }
+
+        showToastMessage("SDL library has been successfully loaded");
+        return true;
+    }
+
+    private void showToastMessage(String message) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
+
 }
