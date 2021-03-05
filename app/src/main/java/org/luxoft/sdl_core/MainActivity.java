02e3bf584ad84436e8903fdc1c33916cc7208f5c
Andrii Kalinich <AKalinich@luxoft.com>
2021-03-05 15:23:51 -0500

    Update MainActivity
    - Added 2 fields for a backward call from NDK
    - Added text fields to display internal and external storage paths

--- app/src/main/java/org/luxoft/sdl_core/MainActivity.java
+++ app/src/main/java/org/luxoft/sdl_core/MainActivity.java
@@ -2,8 +2,10 @@ package org.luxoft.sdl_core;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private boolean is_first_load_ = true;
     private Button start_sdl_button;
     private Button stop_sdl_button;
+    public static String sdl_cache_folder_path;
+    public static String sdl_external_dir_folder_path;
 
     private native static void StartSDL();
     private native static void StopSDL();
@@ -22,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sdl_cache_folder_path = getCacheDir().toString();
+        TextView cache_folder_view = findViewById(R.id.cache_folder);
+        cache_folder_view.setText(String.format("Cache folder: %s", sdl_cache_folder_path));
+
+        sdl_external_dir_folder_path = Environment.getExternalStorageDirectory().toString().concat("/SDL");
+        TextView external_folder_view = findViewById(R.id.external_folder);
+        external_folder_view.setText(String.format("External folder: %s", sdl_external_dir_folder_path));
 
         start_sdl_button = findViewById(R.id.start_sdl_button);
         stop_sdl_button = findViewById(R.id.stop_sdl_button);
