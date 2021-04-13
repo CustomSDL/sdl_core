bb6679765d7eee360983fb523cdb7c3445084a27
Andrii Kalinich <AKalinich@luxoft.com>
2021-04-13 19:46:03 -0400

    Add all required archs
    Also updated gradle task to cover all the architectures and prepare a
    proper fat APK

--- app/src/main/cpp/src/3rd_party-static/encryption/src/hashing.cc
+++ app/src/main/cpp/src/3rd_party-static/encryption/src/hashing.cc
@@ -6,10 +6,11 @@ namespace encryption {
 
 std::string MakeHash(const std::string& input) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha_hasher;
-    SHA256_Init(&sha_hasher);
-    SHA256_Update(&sha_hasher, input.c_str(), input.size());
-    SHA256_Final(hash, &sha_hasher);
+// TODO: Fix OpenSSL
+//    SHA256_CTX sha_hasher;
+//    SHA256_Init(&sha_hasher);
+//    SHA256_Update(&sha_hasher, input.c_str(), input.size());
+//    SHA256_Final(hash, &sha_hasher);
     std::stringstream ss;
     for (size_t i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
